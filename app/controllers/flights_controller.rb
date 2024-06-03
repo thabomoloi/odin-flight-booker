2e34ea9845e64888ca5d7468444bdcb6ea188847
Thabo Vincent Moloi <thabomoloicodes@gmail.com>
2024-06-03 16:23:08 +0200

    feat: add flight search

---
+++ app/controllers/flights_controller.rb
@@ -0,0 +1,15 @@
+class FlightsController < ApplicationController
+  def index
+    @airport_options = Airport.all.map { | airport | airport.code  }
+    @unique_departure_dates = Flight.unique_departure_dates
+    @flight_details = {}
+
+    required_params = [ :departure_code, :arrival_code, :date, :num_tickets ]
+
+    if required_params.all? { |param| params[param].present? }
+      date = Date.parse(params[:date]) rescue nil
+      @flight_details = params.slice(:departure_code, :arrival_code, :date, :num_tickets)
+      @flights = Flight.where("DATE(departure_time) = ?", date)
+    end
+  end
+end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,10 @@
 class Flight < ApplicationRecord
   belongs_to :departure_airport, class_name: "Airport"
   belongs_to :arrival_airport, class_name: "Airport"
+
+  def self.unique_departure_dates
+    select("DISTINCT DATE(departure_time) AS formatted_date")
+      .order("formatted_date")
+      .map { |f| f.formatted_date.to_s }
+  end
 end
