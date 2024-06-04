1fd0e9017d8aee6d38e85aeffb58583975ea1502
Thabo Vincent Moloi <thabomoloicodes@gmail.com>
2024-06-04 05:04:39 +0200

    feat: add flight search results

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -7,9 +7,9 @@ class FlightsController < ApplicationController
     required_params = [ :departure_code, :arrival_code, :date, :num_tickets ]
 
     if required_params.all? { |param| params[param].present? }
-      date = Date.parse(params[:date]) rescue nil
+      @date = Date.parse(params[:date]) rescue nil
       @flight_details = params.slice(:departure_code, :arrival_code, :date, :num_tickets)
-      @flights = Flight.where("DATE(departure_time) = ?", date)
+      @flights = Flight.where("DATE(departure_time) = ?", @date)
     end
   end
 end
--- app/helpers/flights_helper.rb
+++ app/helpers/flights_helper.rb
@@ -1,2 +1,7 @@
 module FlightsHelper
+  def format_duration(minutes)
+    duration = ActiveSupport::Duration.build(minutes * 60)
+    parts = duration.parts.map { |part, value| "#{value}#{part.to_s[0]}" }
+    parts.join
+  end
 end
