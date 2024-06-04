a6b951d5db4b8ed662802915c4b1db10450325bb
Thabo Vincent Moloi <thabomoloicodes@gmail.com>
2024-06-04 09:55:25 +0200

    refactor: make date select box to show different date format
    Change select box data to show users date in formats `%A, %d %B,%Y` instead `%Y-%m-%d`.

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,7 +1,7 @@
 class FlightsController < ApplicationController
   def index
     @airport_options = Airport.all.map { | airport | airport.code  }
-    @unique_departure_dates = Flight.unique_departure_dates
+    @unique_departure_dates =  Flight.unique_departure_dates.map { |d| [ Date.parse(d).strftime("%A, %d %B %Y"), d ] }
     @flight_details = {}
 
     required_params = [ :departure_code, :arrival_code, :date, :num_tickets ]
