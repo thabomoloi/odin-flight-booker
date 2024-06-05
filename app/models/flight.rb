8fc15cf7d7258c8600addbbaa4f95db0b9bd659e
Thabo Vincent Moloi <thabomoloicodes@gmail.com>
2024-06-05 02:15:24 +0200

    add models for passenger and booking

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,6 +1,8 @@
 class Flight < ApplicationRecord
   belongs_to :departure_airport, class_name: "Airport"
   belongs_to :arrival_airport, class_name: "Airport"
+  has_many :bookings
+  has_many :passengers, through: :bookings
 
   def self.unique_departure_dates
     select("DISTINCT DATE(departure_time) AS formatted_date")
