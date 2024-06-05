5c9d7c93efb3e41333759471f86ef37f230b4684
Thabo Vincent Moloi <thabomoloicodes@gmail.com>
2024-06-06 00:19:52 +0200

    feat: add form for booking information

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,13 @@
 class BookingsController < ApplicationController
   def new
+    required_params = [ :flight_id, :num_tickets ]
+    if required_params.all? { |param| params[param].present? }
+      @num_passengers = params[:num_tickets].to_i
+      @flight = Flight.find(params[:flight_id])
+      @booking = Booking.new
+    else
+      redirect_back(fallback_location: root_path)
+    end
   end
   def create
   end
--- app/helpers/flights_helper.rb
+++ app/helpers/flights_helper.rb
@@ -4,4 +4,8 @@ module FlightsHelper
     parts = duration.parts.map { |part, value| "#{value}#{part.to_s[0]}" }
     parts.join
   end
+
+  def format_duration2(minutes)
+    ActiveSupport::Duration.build(minutes * 60).inspect
+  end
 end
